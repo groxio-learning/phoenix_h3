9e90c731ce6d51700f544534b8f6c7fa5c593fa0
Anthony Akpan <anthony.akpan@hiive.com>
2024-11-22 08:49:54 -0800

    improve validation

--- worbee/lib/worbee_web/components/worbee_components.ex
+++ worbee/lib/worbee_web/components/worbee_components.ex
@@ -40,8 +40,8 @@ defmodule WorbeeWeb.WorbeeComponents do
 
   def guess_form(assigns) do
     ~H"""
-    <.simple_form for={@form} phx-submit="make-guess">
-      <.input field={@form[:guess]} />
+    <.simple_form for={@form} phx-change="validate" phx-submit="make-guess">
+      <.input field={@form[:guess]} autocomplete="off"/>
       <.button>submit</.button>
     </.simple_form>
     """
